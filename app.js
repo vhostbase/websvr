22829329935c9b7f7bbf4d20b7220bf11a36445e
vhostbase <vhostbase@gmail.com>
2020-04-08 15:49:04 +0530

    upd

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
-const WebSocketServer = require('ws').Server;
 var express = require('express');
 var app = express();
+var https = require('https');
 app.use(express.static('public'));
+
 app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
 })
-var port = process.env.PORT | 9080
-var sslSrv = app.listen(port, function () {
-   var host = sslSrv.address().address
-   var port = sslSrv.address().port
+app.post('/send', function (req, res) {
+	console.log(req);
+	if(req.url == '/send'){
+	let body = [];
+	req.on('data', (chunk) => {
+	  body.push(chunk);
+	}).on('end', () => {
+	  body = Buffer.concat(body).toString();
+	  // at this point, `body` has the entire request body stored in it as a string
+	  sendNotification(body);
+	});
+	}else{
+		res.sendFile( __dirname + "/" + "index.html" );
+	}
+});
+var server = app.listen(process.env.PORT, function () {
+   var host = server.address().address
+   var port = server.address().port
    console.log("App listening at http://%s:%s", host, port)
 });
-/*wss = new WebSocketServer({server: sslSrv});  
-console.log("WebSocket Secure server is up and running.");
-var clientSet = {};
-wss.on('connection', function (client, incoming_request) {
-	console.log(incoming_request.url.replace('\?', ''));
-	const urlParams = new URLSearchParams(incoming_request.url.replace('/?', ''));
-	const myParam = urlParams.get('yuid');
-	var oldClientId = clientSet[myParam];	
-	clientSet[myParam]=client;
-  console.log("A new WebSocket client was connected.");
-  client.on('message', function (message) {
-	  var msgData = JSON.parse(message);
-	  var yuid = msgData['yuid'];
-    wss.broadcast(message, client, yuid);
-  });
-});
-wss.broadcast = function (data, exclude, yuid) {
-  var i = 0, n = this.clients ? this.clients.length : 0, client = null;
-  if (n < 1) return;
-  console.log("Broadcasting message to all " + n + " WebSocket clients.");
-	for (let [key, value] of Object.entries(clientSet)) {
-		console.log('key :: '+key);
-		var targetClient = value;
-		if(targetClient === exclude) continue;
-		if(yuid === key){			
-			if (targetClient.readyState === targetClient.OPEN) targetClient.send(data);
-				else console.error('Error: the targetClient state is ' + targetClient.readyState);
-		}
-	}
-};*/
+const clientOptions = {
+  hostname: 'fcm.googleapis.com',
+  port: 443,
+  path: '/fcm/send',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+	'Authorization' : 'key=AAAArH0_fRY:APA91bH5-IbDe5KL_MLcX2f6twF7PrRHuauRCN7kbLSBLywn5F6lo3j2v6_YBvN6e-RMIHZFHcKBe8NZHqvEXkR2CEbWIOs9rKLEgL0kG0v3v9hqyE7i5LtCAkViYdAAE06-4W1WNbUB'
+  }
+}
+function sendNotification(body){
+	const clientReq = https.request(clientOptions, res => {
+	  console.log('statusCode: ${res.statusCode}');
+
+	  res.on('data', d => {
+		process.stdout.write(d)
+	  })
+	});
+	clientReq.on('error', error => {
+	  console.error(error)
+	});
+
+	clientReq.write(body);
+	clientReq.end();
+}
