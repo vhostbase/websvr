4b52643eb0f9f5fe409f6d67bf0073c80877e32b
vhostbase <vhostbase@gmail.com>
2020-04-08 15:56:43 +0530

    upd

--- app.js
+++ app.js
@@ -14,3 +14,30 @@ var server = app.listen(process.env.PORT, function () {
 wss = new WebSocketServer({server: server});  
 console.log("WebSocket Secure server is up and running.");
 var clientSet = {};
+wss.on('connection', function (client, incoming_request) {
+	console.log(incoming_request.url.replace('\?', ''));
+	const urlParams = new URLSearchParams(incoming_request.url.replace('/?', ''));
+	const myParam = urlParams.get('yuid');
+	var oldClientId = clientSet[myParam];	
+	clientSet[myParam]=client;
+  console.log("A new WebSocket client was connected.");
+  client.on('message', function (message) {
+	  var msgData = JSON.parse(message);
+	  var yuid = msgData['yuid'];
+    wss.broadcast(message, client, yuid);
+  });
+});
+wss.broadcast = function (data, exclude, yuid) {
+  var i = 0, n = this.clients ? this.clients.length : 0, client = null;
+  if (n < 1) return;
+  console.log("Broadcasting message to all " + n + " WebSocket clients.");
+	for (let [key, value] of Object.entries(clientSet)) {
+		console.log('key :: '+key);
+		var targetClient = value;
+		if(targetClient === exclude) continue;
+		if(yuid === key){			
+			if (targetClient.readyState === targetClient.OPEN) targetClient.send(data);
+				else console.error('Error: the targetClient state is ' + targetClient.readyState);
+		}
+	}
+};
