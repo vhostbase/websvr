be70ee31913d58f143fdb1eb795935a70f898653
vhostbase <vhostbase@gmail.com>
2020-04-08 15:30:51 +0530

    upd

--- app.js
+++ app.js
@@ -22,52 +22,44 @@ app.post('/send', function (req, res) {
 		res.sendFile( __dirname + "/" + "index.html" );
 	}
 });
-var sslSrv = app.listen(process.env.PORT, function () {
+var port = process.env.PORT | 9080
+var sslSrv = app.listen(port, function () {
    var host = sslSrv.address().address
    var port = sslSrv.address().port
    console.log("App listening at http://%s:%s", host, port)
 });
 wss = new WebSocketServer({server: sslSrv});  
 console.log("WebSocket Secure server is up and running.");
+var clientSet = {};
 /** successful connection */
 wss.on('connection', function (client, incoming_request) {
-	var clientId = client._ultron.id;
-	console.log(client.upgradeReq.url.replace('\?', ''));
-	const urlParams = new URLSearchParams(client.upgradeReq.url.replace('/?', ''));
+	console.log(incoming_request.url.replace('\?', ''));
+	const urlParams = new URLSearchParams(incoming_request.url.replace('/?', ''));
 	const myParam = urlParams.get('yuid');
 	var oldClientId = clientSet[myParam];	
-	clientSet[myParam]=clientId;
-  console.log("A new WebSocket client was connected."+clientId);
+	clientSet[myParam]=client;
+  console.log("A new WebSocket client was connected.");
   /** incomming message */
   client.on('message', function (message) {
 	  var msgData = JSON.parse(message);
-	  var id = msgData.yuid;
-	  var tgtClientId = clientSet[id];
+	  var yuid = msgData['yuid'];
     /** broadcast message to all clients */
-    wss.broadcast(message, client, tgtClientId);
+    wss.broadcast(message, client, yuid);
   });
 });
 // broadcasting the message to all WebSocket clients.
-wss.broadcast = function (data, exclude, tgtClientId) {
+wss.broadcast = function (data, exclude, yuid) {
   var i = 0, n = this.clients ? this.clients.length : 0, client = null;
   if (n < 1) return;
   console.log("Broadcasting message to all " + n + " WebSocket clients.");
-  for (i=0; i < n; i++) {
-	client = this.clients[i];
-	console.log("client id "+client._ultron.id);
-	console.log("target id "+tgtClientId);
-	if(data.indexOf('closeConnection') > -1){
-		console.log("colse target id "+client._ultron.id);
-		if (client.readyState === client.OPEN) client.send(data);
-			else console.error('Error: the client state is ' + client.readyState);
-	}else{
-		// don't send the message to the sender...
-		if (client._ultron.id === exclude._ultron.id) continue;
-		if(tgtClientId === client._ultron.id){
-			console.log('Found Client...');
-			if (client.readyState === client.OPEN) client.send(data);
-			else console.error('Error: the client state is ' + client.readyState);
+  //console.log(clientSet);
+	for (let [key, value] of Object.entries(clientSet)) {
+		console.log('key :: '+key);
+		var targetClient = value;
+		if(targetClient === exclude) continue;
+		if(yuid === key){			
+			if (targetClient.readyState === targetClient.OPEN) targetClient.send(data);
+				else console.error('Error: the targetClient state is ' + targetClient.readyState);
 		}
 	}
-  }
 };
