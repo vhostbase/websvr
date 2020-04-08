d94a42becf2ed06df0c0540db333dc26bb3842a1
vhostbase <vhostbase@gmail.com>
2020-04-08 08:59:30 +0530

    upd websocket

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const WebSocketServer = require('ws').Server,
-express = require('express'),
+const WebSocketServer = require('ws').Server;
+/*express = require('express'),
   https = require('https'),
   app = express(),
-  fs = require('fs');
+  fs = require('fs');*/
 var clientSet = {};
-const pkey = fs.readFileSync('./ssl/key.pem'),
+/*const pkey = fs.readFileSync('./ssl/key.pem'),
   pcert = fs.readFileSync('./ssl/cert.pem'),
-  options = {key: pkey, cert: pcert, passphrase: '123456789'};
+  options = {key: pkey, cert: pcert, passphrase: '123456789'};*/
 var wss = null, sslSrv = null;
 // use express static to deliver resources HTML, CSS, JS, etc)
 // from the public folder 
-app.use(express.static('public'));
+/*app.use(express.static('public'));
 
 app.use(function(req, res, next) {
   if(req.headers['x-forwarded-proto']==='http') {
     return res.redirect(['https://', req.get('Host'), req.url].join(''));
   }
   next();
-});
+});*/
 var port = process.env.PORT | 8080;
 // start server (listen on port 443 - SSL)
-sslSrv = https.createServer(options, app).listen(port, function () {
+/*sslSrv = https.createServer(options, app).listen(port, function () {
    var host = sslSrv.address().address
    var port = sslSrv.address().port
    console.log("App listening at http://%s:%s", host, port)
 });
-console.log("The HTTPS server is up and running ");
+console.log("The HTTPS server is up and running ");*/
 
 // create the WebSocket server
-wss = new WebSocketServer({server: sslSrv});  
+wss = new WebSocketServer({port: port});  
 console.log("WebSocket Secure server is up and running.");
 /** successful connection */
 wss.on('connection', function (client, incoming_request) {	
