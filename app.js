bd4e01ef20bc62dfd4f1a0d8d762e6638606c91c
vhostbase <vhostbase@gmail.com>
2020-04-08 15:46:58 +0530

    upd

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const WebSocketServer = require('ws').Server;
 var express = require('express');
 var app = express();
-//var https = require('https');
 app.use(express.static('public'));
-
 app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
 })
